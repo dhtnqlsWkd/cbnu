6f24ba33947b08ab5623ca34748d94bb872cdbd5
CBNU <CBNU@CBNU-PC>
2017-09-22 16:11:11 +0900

    0908

---
+++ 0908_2014041059/src/Character.java
@@ -0,0 +1,20 @@
+
+public class Character {
+
+	public static void main(String[] args) {
+		char ch;
+		ch = 97;
+		System.out.println("ch = " + ch);
+		ch = 'a';
+		System.out.println("ch = " + ch);
+		ch = '\u0061';
+		System.out.println("ch = " + ch);
+		System.out.print("여시는 첫째줄 ");
+		System.out.print("입니다.\n");
+		System.out.println("여기는 둘째줄 입니다.");
+		System.out.print("국어\t영어\t수학\n");
+		System.out.println("--------------------");
+		System.out.println("80\t50\t100");
+	}
+
+}
---
+++ 0908_2014041059/src/Variable.java
@@ -0,0 +1,21 @@
+
+public class Variable {
+
+	public static void main(String[] args) {
+		int a;
+		a = 10;
+		System.out.println("a = " + a);
+		a = 20;
+		System.out.println("a = " + a);
+		System.out.println("a + a = " + (a + a));
+		int b = 30;
+		System.out.println("b = " + b);
+		float f;
+		f = 0.123456789f;
+		System.out.println("f = " + f);
+		double d;
+		d = 0.12345678901234567;
+		System.out.println("d = " + d);
+	}
+
+}
