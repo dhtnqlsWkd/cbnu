ec0eb7254536c8c349fbc28a8f4339fa00ab05a5
CBNU <CBNU@CBNU-PC>
2017-09-29 16:07:59 +0900

    0929

---
+++ 0929_2014041059/src/InstanceVariable.java
@@ -0,0 +1,26 @@
+
+public class InstanceVariable {
+
+	public static void main(String[] args) {
+		Student obj = new Student();
+		
+		obj.m_nEng = 40;
+		obj.m_nMat = 50;
+		
+		System.out.println("obj의 국어점수 = " + obj.m_nKor);
+		System.out.println("obj의 영어점수 = " + obj.m_nEng);
+		System.out.println("obj의 수학점수 = " + obj.m_nMat);
+		System.out.println("Student의 선생님 = " + Student.g_strTeacher);
+		System.out.println("obj의 선생님 = " + obj.g_strTeacher);
+		obj.g_strTeacher = "안중근";
+		System.out.println("obj의 선생님 = " + obj.g_strTeacher);
+	}
+
+}
+
+class Student {
+	int m_nKor;
+	int m_nEng;
+	int m_nMat;
+	static String g_strTeacher = "김구";
+}
